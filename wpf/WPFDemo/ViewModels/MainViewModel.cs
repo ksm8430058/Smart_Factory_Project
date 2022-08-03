dd3c8eb31a67c2eb43dc293313e1edd927ce8244
AellimSun <elim9010@gmail.com>
2022-08-03 12:04:07 +0900

    220803 WPF공부를 위한 WPFDemo 폴더 업로드

---
+++ wpf/WPFDemo/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Caliburn.Micro;
+
+namespace WPFDemo.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+        public MainViewModel()
+        {
+            DisplayName = "SmartFactory Monitoring System v1.0";
+        }
+    }
+}
---
+++ wpf/WPFDemo/Views/MainView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+
+namespace WPFDemo.Views
+{
+    /// <summary>
+    /// MainView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
